445a5495eb4f522256629707af186496e4f61334
bonem97 <bonem1997@gmail.com>
2020-12-11 21:14:43 -0500

    further work

--- exploration.R
+++ exploration.R
@@ -1,6 +1,19 @@
 # Data exploration of global economic freedom
 
-# Import data
-data <- read.csv("economic_freedom_index2019_data.csv", header=TRUE, stringsAsFactors=FALSE)
+# Dependencies
+library("tmap")
+library("tmaptools")
+library("sf")
+library("leaflet")
 
-data.head()
+# Import data
+data <- read.csv("C:\\Users\\bonem\\OneDrive\\Desktop\\R_we_free_yet\\economic_freedom_index2019_data.csv", 
+                 header=TRUE, stringsAsFactors=FALSE)
+head(data)
+
+# How many values are listed per collumn?
+sapply(data, function(x) length(unique(x)))
+# 186 Countries, 5 Regions
+
+GDP_data <- c(data$Country.Name, data$GDP.per.Capita..PPP.)
+print(GDP_data)
